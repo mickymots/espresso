c74b2a216cd9def210138f2aa473edb313c29827
Amit Singh <amit.prakashsingh@gmail.com>
2021-05-25 19:57:02 -0400

    implemented review comments

--- src/reports/views.py
+++ src/reports/views.py
@@ -2,35 +2,25 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.http import HttpResponse
+from django.http import Http404
 from django.template import loader
 from django.core.exceptions import ObjectDoesNotExist 
 
-
 from beans_intake.models import Intake, IntakeNotes, IntakeFiles, Location, Status,  DryingBatch, IntakeDetails, Inventory, InventoryFiles
 from beans_intake.views import BATCH_STATUS_DRYING, BATCH_STATUS_INTAKE, BATCH_STATUS_GRADING, BATCH_STATUS_PARCHMENT, BATCH_STATUS_RESTING
+from hull_grade.models import GradedBeans
+from parchment_intake.models import ParchmentIntake, ParchmentIntakeFiles
 
 intake_details_template = 'intake_details.html/'
+parchment_intake_details_template = 'parchment_intake_details.html'
+
+
+
 
 
 def index(request):
-
     context = {}
-    # query_results = ParchmentIntake.objects.all()
-    # context['query_results'] =  query_results
     return render(request, 'index.html', context=context)
-
-
-
-# def get_batch_with_status():
-#     template = 'dry_coffee/index.html'
-#     context = {}
-#     query_results = IntakeDetails.objects.all().filter(status__in=Status.objects.filter(
-#         id__in=[BATCH_STATUS_INTAKE, BATCH_STATUS_DRYING])).filter(is_active_status=True)
-
-#     context['query_results'] = query_results
-#     context['header'] = 'List Of Intake Batches'
-#     context['is_actionable'] = True
-#     return render(request, template, context=context)
 
 
 
@@ -46,13 +36,16 @@ def get_batch_with_status(request, batch_status):
 
     elif batch_status == 'RESTING':
         status = Status.objects.get(pk=BATCH_STATUS_RESTING)
-
+    elif 'GRADED' in batch_status:
+        query_results = GradedBeans.objects.all()
+        template = 'hulled_graded_report.html'
     
     if status:
         query_results = query_results.filter(status=status)
 
     context['query_results'] = query_results
     context['header'] = f'List Of {batch_status} Batches'
+    context['batch_status'] = batch_status
     context['is_actionable'] = False
     return render(request, template, context=context)
 
@@ -60,14 +53,39 @@ def get_batch_with_status(request, batch_status):
 # Show Intake details
 def get_intake_details(request, batch_status, intake_id):
     try:
-        intake = Intake.objects.get(pk=intake_id)
+        
+        if batch_status == 'PH_GRADED':
+            intake = ParchmentIntake.objects.get(pk=intake_id)
+            uploaded_files = ParchmentIntakeFiles.objects.all().filter(intake=intake)
+            return render(request, parchment_intake_details_template, {'intake': intake, 'uploaded_files': uploaded_files, 'batch_status':batch_status})
+
+        elif batch_status == 'GRADED':
+            inventory = Inventory.objects.get(pk=intake_id)
+            intake = inventory.intake
+        else:
+            intake = Intake.objects.get(pk=intake_id)
+        
+        intake_details, intake_notes, uploaded_files, inventory, inventory_files = get_additonal_details(intake, batch_status)
+    except ObjectDoesNotExist:
+        intake_details = None
+        intake_notes  = None
+        uploaded_files = None
+
+    except Intake.DoesNotExist:
+        raise Http404("Intake does not exist")
+    return render(request, intake_details_template, {'intake': intake, 'intake_details': intake_details, 
+        'intake_notes':intake_notes, 'uploaded_files': uploaded_files, 'inventory': inventory,'inventory_files': inventory_files, 'batch_status':batch_status})
+
+
+def get_additonal_details(intake, batch_status):
+    if batch_status != 'PH_GRADED':
+
         intake_details = IntakeDetails.objects.get(
             is_active_status=True, intake=intake)
         try:
             intake_notes = IntakeNotes.objects.get(intake=intake)
             
         except ObjectDoesNotExist as e:
-            print(e)
             intake_notes = None
         try:
             uploaded_files = IntakeFiles.objects.all().filter(intake=intake)
@@ -79,12 +97,5 @@ def get_intake_details(request, batch_status, intake_id):
         except ObjectDoesNotExist as e:
             inventory = None
             inventory_files =None
-    except ObjectDoesNotExist:
-        intake_details = None
-        intake_notes  = None
-        uploaded_files = None
-
-    except Intake.DoesNotExist:
-        raise Http404("Intake does not exist")
-    return render(request, intake_details_template, {'intake': intake, 'intake_details': intake_details, 
-        'intake_notes':intake_notes, 'uploaded_files': uploaded_files, 'inventory': inventory,'inventory_files': inventory_files, 'batch_status':batch_status})
+    
+        return intake_details, intake_notes, uploaded_files, inventory, inventory_files
